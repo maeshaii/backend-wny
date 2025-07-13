e520a5a1f6ae755d0def8630ae31ec45c9e4acf4
shairame04 <shairamaemaasin04@gmail.com>
2025-07-13 11:24:26 +0800

    Already Has Tracker Form Data

--- backend/settings.py
+++ backend/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'finals',
+        'NAME': 'wny-db',
         'USER': 'postgres',
         'PASSWORD': '12345',
         'HOST': 'localhost',
